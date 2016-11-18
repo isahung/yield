f358355999a89f0debeb13e71ac6de0edb179e7b
Isa Hung <isa19790811@gmail.com>
2016-11-18 16:01:03 +0800

    add the target yield rate as 6.5%

--- yield/yield.py
+++ yield/yield.py
@@ -17,12 +17,16 @@ def main():
   stockid.gen_stockid()
   f = open('stockid.txt', 'r')        
   result = list()
+
+  y = open('yield.txt','a')
+  y.write(" ID   Price  Avg. SD\n")
+
   for line in f.readlines():      
     line = line.strip()
     y = open('yield.txt','a')
     # check the yield rate is > 6.25%
     if parse_stock.get_yield(line)/float(getStock(line)) > 0.0625:
-      y.write(line + str('  ') + getStock(line) + str('  ') + str(parse_stock.get_yield(line)) + str('\n'))
+      y.write(line + str('  ') + getStock(line) + str('   ') + str(parse_stock.get_yield(line)) + str('\n'))
   print('Finish time: {:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()))
 
 if __name__ == "__main__":
